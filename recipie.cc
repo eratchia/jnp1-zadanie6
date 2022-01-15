e51fa44d26672cc58f9904810fb278456b51e623
eratchia <eratchias@gmail.com>
2022-01-15 09:43:41 +0000

    removing helper function

--- recipie.cc
+++ recipie.cc
@@ -4,12 +4,6 @@ using std::initializer_list;
 using std::unique_ptr;
 using std::make_unique;
 
-template<typename TO, typename FROM>
-unique_ptr<TO> static_unique_pointer_cast (unique_ptr<FROM>&& old){
-    return unique_ptr<TO>{static_cast<TO*>(old.release())};
-    //conversion: unique_ptr<FROM>->FROM*->TO*->unique_ptr<TO>
-}
-
 // instruction
 recipie::iterator instruction::begin() const {
     instruction_iterator_impl imp(this, 0);
